a26e2e17357a7cd247f7a8c52b0966d506321380
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-10-21 19:19:29 +0100

    Updated styling and button functionality to digits and some operators

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    setCalc(calc + value)
+  }
 
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button key={i}>{i}</button>
+        <button
+          onClick={() => updateCalc(i.toString())} key={i}>
+          {i}
+        </button>
       )
     }
 
@@ -18,21 +30,22 @@ function App() {
   return (
     <div className="calculator">
       <div className="display">
-        <span> ‎0 ‎‎</span>
+        {result ? <span> ‎0</span> : ' '}{ calc || "0" }
       </div>
 
       <div className="operators">
-        <button>/</button>
-        <button>*</button>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => updateCalc('/')}>/</button>
+        <button onClick={() => updateCalc('*')}>*</button>
+        <button onClick={() => updateCalc('+')}>+</button>
+        <button onClick={() => updateCalc('-')}>-</button>
         <button>del</button>
       </div>
 
       <div className="digits">
         {createDigits()}
-        <button>‎0</button>
-        <button>.</button>
+        <button  onClick={() => updateCalc('0')}>‎0</button>
+        <button onClick={() => updateCalc('.')}>.</button>
+        
         <button>=</button>
       </div>
 
